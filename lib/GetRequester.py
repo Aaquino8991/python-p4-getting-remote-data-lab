1d1463d9a6cc791b875cc996771b66b1bcf710d2
Anthony <aquinoanthony851@gmail.com>
2024-05-03 16:05:05 -0600

    Getting remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,23 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
+        # import ipdb; ipdb.set_trace()
+        self.get_response_body()
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        people_list = []
+        people = json.loads(self.get_response_body())
+        for person in people:
+            people_list.append(person)
+        return people_list
+
+url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+people = GetRequester(url)
+people = people.load_json()
+
+for person in people:
+    print(person)
